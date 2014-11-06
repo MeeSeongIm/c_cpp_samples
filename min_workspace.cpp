9b18388a9e53df77a4bc09ebb6a5fbf9615f1c13
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 11:57:39 -0600

    Create min_workspace.cpp

---
+++ min_workspace.cpp
@@ -0,0 +1,14 @@
+// min possible C++ workspace.  
+
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char ** argv ) {
+	cout << "This is the first line to be printed to the Console. \n";
+	cout << "This is the second line to be printed to the Console. \n";
+	return 0;
+}
+
+
+
