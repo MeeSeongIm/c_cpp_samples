92ff306e9770de77b577fa2f065aaf03f1881ca5
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 13:32:15 -0600

    Update characters_of_string.c

--- characters_of_string.c
+++ characters_of_string.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 
 int main( int argc, char ** argv ) {
-	int x = printf("This is a sample code involving printing the number of characters in this line.\n");
-	printf("printf returned %d.\n", x);
-	printf("\n");
-	int y = printf("The number of chars in this line is...\n");
-	printf("%d.\n", y);
+	for (int i = 1; i <= 10; i++) {
+		printf("The square of %d is %d.\n", i, i*i);
+		printf("\n");
+	}
+		int x = printf("This is a sample code involving printing the number of characters in this line.\n");
+		printf("printf returned %d.\n", x);
+		printf("\n");
+		int y = printf("The number of chars in this line (times 100) is...\n")*100;
+		printf("%d.\n", y);
 	return 0;
 }
 
