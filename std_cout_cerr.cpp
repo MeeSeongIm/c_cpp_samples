8aa580076381ab08c41c2899ae5cd264015c6056
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 10:25:32 -0600

    Create std_cout_cerr.cpp

---
+++ std_cout_cerr.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+//using namespace std;
+
+int main(int argc, char ** argv) {
+	std::cout << "Print this to the Console." << std::endl;
+	std::cerr << "Red font.\n";
+	std::cout << "Printed lines not necessary in order. " << std::endl;
+	return 0;
+}
+
+
