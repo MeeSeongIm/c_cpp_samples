078232d364ab22ad569fd04254e02794a1b87500
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 10:22:19 -0600

    Create stdout_stderr.cpp

---
+++ stdout_stderr.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char ** argv) {
+	cout << "Print this to the Console." << endl;
+	cerr << "Red font.\n";
+	cout << "Printed lines not necessary in order. " << endl;
+	return 0;
+}
+
+
