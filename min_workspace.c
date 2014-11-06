5b911fd648d12b0fe42411437cf25b7f28dedb34
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 11:53:50 -0600

    Create min_workspace.c

---
+++ min_workspace.c
@@ -0,0 +1,16 @@
+// minimal possible correct C program.
+
+#include <stdio.h>
+
+int main(int argc, char ** argv){
+	printf("This is the first line printed to the Console. \n");
+	printf("This is the second line printed to the Console. \n");
+	return 0;
+}
+
+/* 	Un-executed line.
+ 	  Un-executed line.
+ */
+
+
+
