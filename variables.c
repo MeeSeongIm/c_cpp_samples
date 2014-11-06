9b5f7c4f3e7c23a2c7672edcbf75462648b2fac9
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 17:13:20 -0600

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main( int argc, char ** argv ) {
+	int i = 7;  							// declare an integer variable
+	printf("%d\n", 2*i);
+	char * s = "This is a sample file.\n";
+	printf(s);
+	for (i=0; i<=11; i++) {
+	printf("%d\n", 2*i+1);
+	}
+	return 0;
+}
+
+
+
