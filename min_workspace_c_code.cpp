abb9ba673d34df5543cb5f32957d6f94c82e7fe8
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 12:51:52 -0600

    Create min_workspace_c_code.cpp

---
+++ min_workspace_c_code.cpp
@@ -0,0 +1,15 @@
+// scripts are the same as a C code but we are running this script through the C++ compiler.
+
+
+#include <cstdio>  // a part of the C standard library, not the designated C++ standard library
+// using namespace std;
+
+int main( int argc, char ** argv ) {
+	printf("This is the first line to be printed to the Console. \n");
+	printf("This is the second line to be printed to the Console. \n");
+	return 0;
+}
+
+
+
+
