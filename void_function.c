3477c17733a9de24fe022c65be742e6d116c65bf
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-07 14:47:29 -0600

    Create void_function.c

---
+++ void_function.c
@@ -0,0 +1,18 @@
+// functions
+
+
+#include <stdio.h>
+										// forward declaration with function signature.
+void func();  							// func() declared. Does not return a value, takes no arguments
+
+int main( int argc, char ** argv ) {
+	func();
+	return 0;
+}
+
+void func() {							// func() defined
+	printf("this is func().\n");
+}
+
+
+
