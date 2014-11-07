f89493927d851e8ffd5f22843ce941823bf4afb1
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 22:59:09 -0600

    Create include.c

---
+++ include.c
@@ -0,0 +1,16 @@
+//  #include directives
+
+
+#include <stdio.h> 				// a standard library header file.
+#include "include.h"			//header file specific to a project or a source file.
+
+int main( int argc, char ** argv ) {
+	printf("the constant is %d\n", _iOne);
+	return 0;
+}
+
+
+
+
+
+
