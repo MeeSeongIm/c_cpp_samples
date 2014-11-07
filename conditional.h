8584a7f607673e0afd69b3ba8620ec3f9791cc9f
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-06 23:06:10 -0600

    Create conditional.h

---
+++ conditional.h
@@ -0,0 +1,20 @@
+
+#ifndef CONDITIONAL_H_
+#define CONDITIONAL_H_
+
+//#ifdef FOO
+#if defined(FOO)
+
+#define NUMBER 47
+
+#else
+
+#define NUMBER 2
+
+#endif
+
+#endif /* CONDITIONAL_H_ */
+
+
+
+
