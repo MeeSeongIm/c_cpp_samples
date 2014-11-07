7fa82785eb4ee00719a7a8ef0b2ff2e7aa564f1b
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-07 00:08:00 -0600

    Create includeA.h

---
+++ includeA.h
@@ -0,0 +1,21 @@
+
+
+// use include guard
+
+// #pragma once // supported by GCC, Microsoft C, Apple's LLVM
+
+#ifndef _INCLUDE_A
+#define _INCLUDE_A
+
+
+struct structA {
+	int member;
+};
+
+
+#endif /* _INCLUDE_A */
+
+
+
+
+
