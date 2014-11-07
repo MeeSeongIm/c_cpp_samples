213a304c5cb4fcb7b412f8deb269b2ae20858134
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-07 00:07:16 -0600

    Create include_guard.c

---
+++ include_guard.c
@@ -0,0 +1,16 @@
+
+// use include guard
+
+
+#include <stdio.h>
+#include "includeA.h"
+#include "includeB.h"
+
+int main( int argc, char ** argv ) {
+	printf("Including multiple files only once. \n");
+	return 0;
+}
+
+
+
+
