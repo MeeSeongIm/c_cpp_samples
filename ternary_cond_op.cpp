11d0af0a04355c7360fcf74bee3a1a7162bc166d
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-07 12:53:21 -0600

    Create ternary_cond_op.cpp

---
+++ ternary_cond_op.cpp
@@ -0,0 +1,20 @@
+// ternary conditional operator.
+// cannot be overloaded.
+
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char ** argv ) {
+	int i = 2;
+	int j = 45;
+	printf("is the condition true? %s",
+			i < j ? "yes": "no");
+
+	return 0;
+}
+
+
+
+
+
