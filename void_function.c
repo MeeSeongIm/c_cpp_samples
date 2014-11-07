dac3c401951ef2e509d304504571f1b0add318fa
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-07 14:48:15 -0600

    Update void_function.c

--- void_function.c
+++ void_function.c
@@ -2,15 +2,15 @@
 
 
 #include <stdio.h>
-										// forward declaration with function signature.
-void func();  							// func() declared. Does not return a value, takes no arguments
+					// forward declaration with function signature.
+void func();  				// func() declared. Does not return a value, takes no arguments
 
 int main( int argc, char ** argv ) {
 	func();
 	return 0;
 }
 
-void func() {							// func() defined
+void func() {				// func() defined
 	printf("this is func().\n");
 }
 
