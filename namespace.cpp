ad733b98ef94145cf2b6e9c12cbaf8c4cd7046ca
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-08 07:43:12 -0600

    Create namespace.cpp

---
+++ namespace.cpp
@@ -0,0 +1,47 @@
+// namespace
+#include <iostream>
+
+namespace BWString {
+const std::string bws = "This is BWString::string";
+class string {
+	std::string s;
+public:
+	string ( void ) : s(bws) {};						// constructor
+	string ( const std::string & _s ) : s(bws) {};		// constructor
+	operator std::string & ( void ) { return s; };
+};
+};	// namespace BWString
+
+
+namespace ThisString {
+const std::string thisString = "This is a string inside a namespace.";
+class string {
+	std::string t;
+public:
+	string ( void ) : t(thisString) {};						// constructor
+	string ( const std::string & _t ) : t(thisString) {};		// constructor
+	operator std::string & ( void ) { return t; };
+};
+};	// namespace ThisString
+
+
+
+//using namespace std;
+//using BWString::string;
+using namespace ThisString;
+string s3("This is a string.");
+
+
+BWString::string s1("testing, testing.");
+std::string s2("This is a sentence.");
+
+int main( int argc, char ** argv ) {
+	std::string s = s1;
+	std::string t = s3;
+	std::cout << s << std::endl;
+	std::cout << t << std::endl;
+	return 0;
+}
+
+
+
