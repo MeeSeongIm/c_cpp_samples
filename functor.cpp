912b69ec77e64adb16cbd90ea69950110a31ad44
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-08 12:51:22 -0600

    Update functor.cpp

--- functor.cpp
+++ functor.cpp
@@ -13,11 +13,20 @@ public:
 	int operator () ( int n ) { return mult * n; }
 };
 
+class DivideBy {
+	int divide;
+public:
+	DivideBy ( int n ) : divide(n) {}
+	int operator () ( int n ) { return divide / n; }  // returns only the integral part after the division
+};
+
+
 int main( int argc, char ** argv ) {
 	MultBy times4(4);
 	MultBy times10(10);
 	MultBy times15(15);
 	MultBy times9(9);
+	DivideBy divided8(8);
 	cout << "times4(5) is " << times4(5) << endl;
 	cout << "times4(15) is " << times4(15) << endl;
 	cout << "times10(5) is " << times10(5) << endl;
@@ -26,6 +35,11 @@ int main( int argc, char ** argv ) {
 	cout << "times15(15) is " << times15(15) << endl;
 	cout << "times9(3) is " << times9(3) << endl;
 	cout << "times9(-3) is " << times9(-3) << endl;
+	cout << "divided8(3) is " << divided8(3) << endl;
+	cout << "divided8(-2) is " << divided8(-2) << endl;
+	cout << "divided8(-1) is " << divided8(-1) << endl;
 	return 0;
 }
+
+
 
