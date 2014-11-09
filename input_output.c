c9cadeba726dca3bf0bbea3f92ed819f6f93e904
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-08 21:40:02 -0600

    Create input_output.c

---
+++ input_output.c
@@ -0,0 +1,34 @@
+// file-io
+
+#include <stdio.h>
+
+int main( int argc, char ** argv ) {
+	const char * fn = "test.file";			// file name test.file
+	const static int maxString = 1024;		// read buffer size
+	const char * str = "This is a literal C-string.\n";
+
+	// create/write the file
+	printf("writing file\n");
+	FILE * fw = fopen(fn, "w");  			// FILE== structure for file handle, (filename, mode string)
+	for(int i = 0; i < 5; i++) {
+		fputs(str, fw);						// fputs == line-oriented write
+	}
+	fclose(fw);
+	printf("done.\n");
+
+	// read the file
+	printf("reading file\n");
+	char buf[maxString];					// buffer
+	FILE * fr = fopen(fn, "r");				// fr == file handle open for read
+	char * rc;
+												 // double paraenthesis: assigning and testing in the same expression in the while-loop
+	while(( rc = fgets(buf, maxString, fr) )) {  // fgets is the complement to fputs to read the file, fgets reads one line at a time
+		printf(buf);
+	}
+	fclose(fr);
+	printf("done.\n");
+
+	return 0;
+}
+
+
