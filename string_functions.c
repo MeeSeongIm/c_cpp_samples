c3ed8a5a72a43d84a4989a0df242933ee3663796
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-09 00:37:40 -0600

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -17,29 +17,29 @@ int main( int argc, char ** argv ) {
 	printf("s1 is %s; s2 is %s\n", s1, s2);
 	printf("sizeof sd1 is %ld; sizeof sd2 is %ld\n", sizeof sd1, sizeof sd2);
 
-	strncpy(sd1, s1, maxBuf);	// strncpy -- copy a string
+	strncpy(sd1, s1, maxBuf);			// strncpy -- copy a string
 	printf("sd1 is %s\n", sd1);
 	strncpy(sd2, s2, maxBuf);
 	printf("sd2 is %s\n", sd2);
 
 	strncat(sd1, " - ", maxBuf - strlen(sd1) - 1);	// strncat -- concatenate strings
-	strncat(sd1, s2, maxBuf - strlen(sd1) - 1);    // arguments = (destination string, the string concatenated onto the end of it, the number of char that one wants to copy)
+	strncat(sd1, s2, maxBuf - strlen(sd1) - 1);    	// arguments = (destination string, the string concatenated onto the end of it, the number of char that one wants to copy)
 	printf("sd1 is %s\n", sd1);
 
 	printf("length of sd1 is %ld\n", strlen(sd1));	// strlen -- get length of string
 	printf("length of sd2 is %ld\n", strlen(sd2));
 
-	i = strcmp(sd1, sd2);	// strcmp -- compare strings. return 0 if the strings compare, returns a nonzero number if they are greater or less than each other.
+	i = strcmp(sd1, sd2);				// strcmp -- compare strings. return 0 if the strings compare, returns a nonzero number if they are greater or less than each other.
 	printf("sd1 %s sd2 (%d)\n", (i == 0) ? "==" : "!=", i);
 	i = strcmp(sd2, s2);
 	printf("sd2 %s s2 (%d)\n", (i == 0) ? "==" : "!=", i);
 
 	c = 'g';
-	cp = strchr(sd1, c);	// strchr -- find a char in string
+	cp = strchr(sd1, c);				// strchr -- find a char in string
 	printf("Did we find a '%c' in sd1? %s\n", c, cp ? "yes" : "no");
 	if(cp) printf("The first '%c' in sd1 is at position %ld\n", c, cp - sd1);
 
-	cp = strstr(sd1, s2);	// strstr -- find a string in string
+	cp = strstr(sd1, s2);				// strstr -- find a string in string
 	printf("Did we find '%s' in sd1? %s\n", s2, cp ? "yes" : "no");
 	if(cp) printf("The first '%s' in sd1 is at position %ld\n", s2, cp - sd1);
 
